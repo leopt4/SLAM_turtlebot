5a5c4bac4785ac6d41ceff22c21c25dc1259ff5f
LeoPhamVN <phamthanhloc.bkhn@gmail.com>
2024-04-14 16:12:14 +0200

    Finished EKF_node. Added odometry and magnetometer classes. They are sensor classes include properties and methods dealing with sensor data. This version runs EKF filter based on odometry frequency.

--- src/turtlebot_teleop_convert_node.py
+++ src/turtlebot_teleop_convert_node.py
@@ -9,8 +9,8 @@ class VelocityConverter:
         rospy.init_node('velocity_converter')
         self.cmd_vel_sub = rospy.Subscriber('/cmd_vel', Twist, self.cmd_vel_callback)
         self.wheel_vel_pub = rospy.Publisher('/turtlebot/kobuki/commands/wheel_velocities', Float64MultiArray, queue_size=10)
-        self.d = 0.23  # Distance between wheels (example value, adjust according to your robot)
-        self.r = 0.033 # Radius of wheels (example value, adjust according to your robot)
+        self.d = 0.235   # Distance between wheels (example value, adjust according to your robot)
+        self.r = 0.035   # Radius of wheels (example value, adjust according to your robot)
 
     def cmd_vel_callback(self, msg):
         v = msg.linear.x
