393f16da4b1cb43ba9d4a07d2b1b7851faf94329
LeoPhamVN <phamthanhloc.bkhn@gmail.com>
2024-04-16 02:23:38 +0200

    add IMU_node implemented by cpp. This node handles IMU data

---
+++ build/CMakeFiles/IMU_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IMU_node.dir/src/IMU_node.cpp.o"
+  "devel/lib/turtlebot_graph_slam/IMU_node"
+  "devel/lib/turtlebot_graph_slam/IMU_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IMU_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
